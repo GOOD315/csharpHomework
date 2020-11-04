69646f8f5b4458423c512433ffb543003e0661dd
GOOD315 <dimababoshin@mail.ru>
2020-11-04 16:35:47 +0300

    залил дз №3

---
+++ homework3_1/homework3_1/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3_1
+{
+    class Complex
+    // Бабошин
+    /* 
+     * а) Дописать структуру Complex, добавив метод вычитания комплексных чисел.Продемонстрировать работу структуры.
+       б) Дописать класс Complex, добавив методы вычитания и произведения чисел.Проверить работу класса.
+       в) Добавить диалог с использованием switch демонстрирующий работу класса.
+    */
+
+    {
+        // Поля приватные.
+        private double im;
+        // По умолчанию элементы приватные, поэтому private можно не писать.
+        double re;
+
+        // Конструктор без параметров.
+        public Complex()
+        {
+            im = 0;
+            re = 0;
+        }
+
+        // Конструктор, в котором задаем поля.    
+        // Специально создадим параметр re, совпадающий с именем поля в классе.
+        public Complex(double re, double _im)
+        {
+            // Здесь имена не совпадают, и компилятор легко понимает, что чем является.              
+            im = _im;
+            // Чтобы показать, что к полю нашего класса присваивается параметр,
+            // используется ключевое слово this
+            // Поле параметр
+            this.re = re;
+        }
+        public Complex Plus(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.im = x2.im + im;
+            x3.re = x2.re + re;
+            return x3;
+        }
+        public Complex Minus(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.im = im - x2.im;
+            x3.re = re - x2.re;
+            return x3;
+        }
+        public Complex Multiply(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.re = re * x2.re - im * x2.im;
+            x3.im = re * x2.im + im * x2.re;
+
+            return x3;
+        }
+        public Complex Divide(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.re = (re * x2.re + im * x2.im) / (Math.Pow(x2.re, 2) + Math.Pow(x2.im, 2));
+            x3.im = (im * x2.re - re * x2.im) / (Math.Pow(x2.re, 2) + Math.Pow(x2.im, 2));
+            x3.re = Math.Round(x3.re, 1);
+            x3.im = Math.Round(x3.im, 1);
+            return x3;
+        }
+        // Свойства - это механизм доступа к данным класса.
+        public double Im
+        {
+            get { return im; }
+            set
+            {
+                // Для примера ограничимся только положительными числами.
+                if (value >= 0) im = value;
+            }
+        }
+        // Специальный метод, который возвращает строковое представление данных.
+        // В методичке в этом методе нет override, у меня программа на это ругалась
+        public override string ToString()
+        {
+            string str = "";
+
+            if (re != 0) str += re;
+            if (im != 0)
+            {
+                if ((re != 0) && (im > 0)) str += "+" + im + "i";
+                else
+                {
+                    str += im + "i";
+                }
+            }
+            if (str.Length == 0) str = "0";
+            return str;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            Boolean exit = false;
+
+            while (exit == false)
+            {
+                Console.WriteLine("Первое комплексное число a+bi");
+                Console.WriteLine("Введите a: ");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите b: ");
+                b = Convert.ToDouble(Console.ReadLine());
+                Complex complex1 = new Complex(a, b);
+
+                Console.WriteLine("Второе комплексное число a+bi");
+                Console.WriteLine("Введите a: ");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите b: ");
+                b = Convert.ToDouble(Console.ReadLine());
+                Complex complex2 = new Complex(a, b);
+
+                Console.WriteLine(@"Введите цифру требуемой операции с этими числами:
+1 - Сложение.
+2 - Вычитание.
+3 - Умножение.
+4 - Деление.");
+                Complex result = null;
+                string strResult = "";
+                int caseSwitch = Convert.ToInt32(Console.ReadLine());
+                switch (caseSwitch)
+                {
+                    case 1:
+                        strResult = "сложения";
+                        result = complex1.Plus(complex2);
+                        break;
+                    case 2:
+                        strResult = "вычитания";
+                        result = complex1.Minus(complex2);
+                        break;
+                    case 3:
+                        strResult = "умножения";
+                        result = complex1.Multiply(complex2);
+                        break;
+                    case 4:
+                        strResult = "деления";
+                        result = complex1.Divide(complex2);
+                        break;
+                }
+                Console.WriteLine($"Результат выполнения операции {strResult}: {result}");
+                Console.WriteLine("Нажмите любую клавишу что бы начать заного, или нажмите 0 что бы выйти из программы");
+                if (Convert.ToInt32(Console.ReadLine()) == 0) exit = true; else exit = false;
+            }
+        }
+
+    }
+}
---
+++ homework3_1/homework3_3/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3_3
+{
+    // Бабошин
+    /*
+    3. *Описать класс дробей — рациональных чисел, являющихся отношением двух целых чисел. 
+    Предусмотреть методы сложения, вычитания, умножения и деления дробей. 
+    Написать программу, демонстрирующую все разработанные элементы класса.
+    * Добавить свойства типа int для доступа к числителю и знаменателю;
+    * Добавить свойство типа double только на чтение, чтобы получить десятичную дробь числа;
+    Добавить проверку, чтобы знаменатель не равнялся 0. Выбрасывать исключение ArgumentException("Знаменатель не может быть равен 0");
+    * Добавить упрощение дробей.
+   */
+    class Fraction
+    {
+        int num { get; set; }
+        int den { get; set; }
+
+        public Fraction(int num, int den = 1)
+        {
+            if (den == 0) throw new ArgumentException("В знаменателе не может быть нуля");
+            this.num = num;
+            this.den = den;
+        }
+
+        public static Fraction operator +(Fraction a) { return a; }
+        public static Fraction operator -(Fraction a) { return new Fraction(-a.num, a.den); }
+        public static Fraction operator +(Fraction a, Fraction b)
+        { return new Fraction(a.num * b.den + b.num * a.den, a.den * b.den); }
+
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            return a + (-b);
+        }
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            return new Fraction(a.num * b.num, a.den * b.den);
+        }
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            if (b.num == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return new Fraction(a.num * b.den, a.den * b.num);
+        }
+        public override string ToString()
+        {
+            Simplify();
+            if (Math.Abs(den) == 1) return $"{num}";
+            else return $"{num}/{den}";
+        }
+        public void Simplify()
+        {
+            // взял по модулю, потому что 
+            int a = Math.Abs(num);
+            int b = Math.Abs(den);
+            while ((a != 0) && (b != 0))
+            {
+                if (a > b)
+                    a = a % b;
+                else
+                    b = b % a;
+            }
+            num = num / (a + b);
+            den = den / (a + b);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean exit = false;
+
+            while (exit == false)
+            {
+                Console.WriteLine("Введите числитель первой дроби: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите знаменатель первой дроби: ");
+                int den1 = Convert.ToInt32(Console.ReadLine());
+                Fraction frac1 = new Fraction(num1, den1);
+
+                Console.WriteLine("Введите числитель второй дроби: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите знаменатель второй дроби: ");
+                int den2 = Convert.ToInt32(Console.ReadLine());
+                Fraction frac2 = new Fraction(num2, den2);
+
+                Console.WriteLine(@"Введите цифру требуемой операции с этими числами:
+1 - Сложение.
+2 - Вычитание.
+3 - Умножение.
+4 - Деление.");
+                int caseSwitch = Convert.ToInt32(Console.ReadLine());
+                switch (caseSwitch)
+                {
+                    case 1:
+                        Console.WriteLine($"результат сложения: {frac1 / frac2}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"результат вычитания: {frac1 / frac2}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"результат умножения: {frac1 / frac2}");
+                        break;
+                    case 4:
+                        Console.WriteLine($"результат деления: {frac1 / frac2}");
+                        break;
+                }
+                Console.WriteLine("Нажмите любую клавишу что бы начать заного, или нажмите 0 что бы выйти из программы");
+                if (Convert.ToInt32(Console.ReadLine()) == 0) exit = true; else exit = false;
+                Console.Clear();
+            }
+        }
+    }
+}
