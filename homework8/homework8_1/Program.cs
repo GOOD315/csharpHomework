04b6e698dfda342440dfd7a8acccb7d92bb1d55b
GOOD315 <dimababoshin@mail.ru>
2020-11-24 17:43:08 +0300

    дз №8

---
+++ homework8/homework8_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework8_1
+{
+    // Бабошин
+    // 1. С помощью рефлексии выведите все свойства структуры DateTime
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t = typeof(DateTime);
+            foreach (var prop in t.GetProperties())
+                Console.WriteLine(prop.Name);
+            Console.ReadLine();
+        }
+    }
+}
